40b4f45a6a0d8e5eaa3713a2c129f8786798767f
Shantanu Shukla <shantanu.shukla10@gmail.com>
2024-06-03 14:43:30 +0530

    fix: which-key help popup with noice.nvim

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,12 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize which-key
+
+---@type LazySpec
+return {
+  "folke/which-key.nvim",
+  opts = {
+    show_help = false,
+    show_keys = false,
+  },
+}
